7b3fc679d53a952912ec7d0c4bc1b9e50dafb9a3
Kyliann <92451561+NanoLoriginal@users.noreply.github.com>
2022-02-07 13:35:29 +0100

    update de brique

--- src/brique.js
+++ src/brique.js
@@ -1,6 +1,11 @@
 class Brique{
-    constructor(Tableau1){
-        this.scene = Tableau1
+    constructor(x,y,type,Tableau1){
+        this.scene = Tableau1;
+        this.briqueType = type;
+        this.brique=this.scene.physics.add.sprite(x,y,'carre').setOrigin(0.0)
+        this.brique.body.setSize(60,30)
+        this.brique.setImmovable(true)
+
     }
 
 
