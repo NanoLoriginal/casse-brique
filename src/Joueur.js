1377c39968979c20cc8e92077ffd0b3e79e224e4
Kyliann <92451561+NanoLoriginal@users.noreply.github.com>
2022-02-07 13:17:55 +0100

    début de Joueur

---
+++ src/Joueur.js
@@ -0,0 +1,12 @@
+class Joueur{
+
+    constructor(Tableau1,x){
+        this.scene = Tableau1;
+        this.playerpad=this.scene.physics.add.sprite(x,310,'carre').setOrigin(0.0)
+    }
+
+
+
+
+
+}
