312a21209903fd6c88006319664c8acafd3c2724
Kyliann <92451561+NanoLoriginal@users.noreply.github.com>
2022-02-07 13:23:57 +0100

    méthodes de déplacement du joueur

---
+++ src/Controle.js
@@ -0,0 +1,16 @@
+class Controle {
+    constructor(Tableau1){
+        this.scene = Tableau1
+        this.InitKeyboard()
+    }
+
+    /*
+     * création de la méthode pour utiliser le clavier
+     */
+
+    InitKeyboard(){
+
+
+
+    }
+}
--- src/Joueur.js
+++ src/Joueur.js
@@ -2,9 +2,22 @@ class Joueur{
 
     constructor(Tableau1,x){
         this.scene = Tableau1;
-        this.playerpad=this.scene.physics.add.sprite(x,310,'carre').setOrigin(0.0)
+        this.playerpad=this.scene.physics.add.sprite(x,700,'carre').setOrigin(0.0);
+    }
+    droite(){
+        this.playerpad.setVelocityX(450)
+    }
+    gauche(){
+        this.playerpad.setVelocityX(-450)
     }
 
+    immobile(){
+        this.playerpad.setVelocityX(0)
+    }
+
+    limite(){
+
+    }
 
 
 
