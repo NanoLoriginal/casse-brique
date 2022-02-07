0aaa0c8851bbf9b680daedbb0f6f1ed4f2f11992
Kyliann <92451561+NanoLoriginal@users.noreply.github.com>
2022-02-07 13:43:25 +0100

    update de main1.js

--- src/main1.js
+++ src/main1.js
@@ -1,32 +1,17 @@
-let gameOptions = {
-    circleColors: [0xff0099, 0xf3f315, 0x83f52c, 0x630dd0]}
 let gameConfig = {
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        min: {
-            width: 800,
-            height: 600
-        },
-        max: {
-            width: 1280,
-            height: 720,
-        }
-    },
-    backgroundColor: '#000000',
-    audio: {
-        disableWebAudio: true
-    },
+    backgroundColor: '#00000',
     parent: 'game',
+    disableWebAudio: true,
     physics: {
         default: 'arcade',
         arcade: {
-            debug:false
+            gravity: { y: 200 }
         }
     },
     scene: new Tableau1()
+
 };
 let game = new Phaser.Game(gameConfig);
