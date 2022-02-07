01d8f17f5363593e5130d54fb71b21e838e966c5
Kyliann <92451561+NanoLoriginal@users.noreply.github.com>
2022-02-07 13:14:14 +0100

    ajout du preload,create etc

---
+++ src/Tableau1.js
@@ -0,0 +1,18 @@
+class Tableau1 extends Phaser.Scene{
+
+    preload(){
+        this.load.image('carre','assets/carre.png');
+        this.load.image('cercle','assets/cercle.png');
+
+    }
+
+    create(){
+
+    }
+
+    update(){
+
+    }
+
+
+}
